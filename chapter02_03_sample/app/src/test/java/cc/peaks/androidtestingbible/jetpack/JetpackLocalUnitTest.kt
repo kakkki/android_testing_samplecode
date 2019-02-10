895d1dfe2999815b49f00d6c73d8ed6f1f88003b
kakehi-binc <kakehi@binc.jp>
2019-02-10 20:58:48 +0900

    update sdk version to 28

--- chapter02_03_sample/app/src/test/java/cc/peaks/androidtestingbible/jetpack/JetpackLocalUnitTest.kt
+++ chapter02_03_sample/app/src/test/java/cc/peaks/androidtestingbible/jetpack/JetpackLocalUnitTest.kt
@@ -1,17 +1,19 @@
 package cc.peaks.androidtestingbible.jetpack
 
-import androidx.test.InstrumentationRegistry
+//import androidx.test.InstrumentationRegistry
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.runner.AndroidJUnit4
-import cc.peaks.androidtestingbible.R
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
+
+import cc.peaks.androidtestingbible.R
+import org.assertj.core.api.Assertions.assertThat
 
 @RunWith(AndroidJUnit4::class)
 class JetpackLocalUnitTest {
   @Test
   fun gettingContextTest() {
-    val context = InstrumentationRegistry.getTargetContext()
+    val context = InstrumentationRegistry.getInstrumentation().context
     val appName = context.getString(R.string.app_name)
     assertThat(appName).isEqualTo("AndroidTestingBible")
   }
